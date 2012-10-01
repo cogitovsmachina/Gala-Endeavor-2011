db08f5b014579ddd3dbe9d6e3dffcd3097033944
Enrique Diaz <nrikediaz@androidtitlan.org>
2012-10-01 13:56:51 -0500

    Minor refactor, changing HomeActivity style

--- src/com/androidtitlan/endeavorsubasta/io/DoBiddingTask.java
+++ src/com/androidtitlan/endeavorsubasta/io/DoBiddingTask.java
@@ -9,16 +9,19 @@ import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
-public class DoBiddingTask extends AsyncTask<String, String, ResponseFromHttpPost> {
+public class DoBiddingTask extends
+		AsyncTask<String, String, ResponseFromHttpPost> {
 	private String mUser;
 	private Activity mActivity;
 	private String mPrice;
 	private String mProduct;
-	
+	private final ProgressDialog dialog = new ProgressDialog(mActivity);
+
 	/**
 	 * constructor
 	 */
-	public DoBiddingTask(String user, String price, String product, Activity activity){
+	public DoBiddingTask(String user, String price, String product,
+			Activity activity) {
 		mUser = user;
 		mProduct = product;
 		mPrice = price;
@@ -27,11 +30,13 @@ public class DoBiddingTask extends AsyncTask<String, String, ResponseFromHttpPos
 
 	@Override
 	protected void onPreExecute() {
+		dialog.setMessage("Pujando ahora, por favor espera...");
+		dialog.show();
 		super.onPreExecute();
 	}
 
 	@Override
-    protected ResponseFromHttpPost doInBackground(String... params) {
+	protected ResponseFromHttpPost doInBackground(String... params) {
 		ResponseFromHttpPost response = null;
 		try {
 			response = WebServices.sendBidding(mUser, mPrice, mProduct);
@@ -40,58 +45,90 @@ public class DoBiddingTask extends AsyncTask<String, String, ResponseFromHttpPos
 		}
 		return response;
 	}
-            
 
-    /**
-     * on getting result
-     */
-    
-    protected void onPostExecute(ResponseFromHttpPost response) {
-    	//AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
-    	try {
-			if (response.getStatusCode() == 200) {
-				/*builder.setMessage("Se ha enviado la oferta al servidor")
-				.setCancelable(false)
-				.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						dialog.cancel();
-					}
-				});*/
-				Toast.makeText(mActivity, "Se ha enviado la oferta al servidor", Toast.LENGTH_LONG).show();
-			}
-			if (response.getStatusCode() == 400) {
-				/*builder.setMessage("Tu oferta no ha podido ser enviada, intenta nuevamente")
-				.setCancelable(false)
-				.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						dialog.cancel();
-					}
-				});*/
-				Toast.makeText(mActivity, "Tu oferta no ha podido ser enviada, intenta nuevamente", Toast.LENGTH_LONG).show();
-			}
-			if (response.getStatusCode() == 500) {
-				/*builder.setMessage("El usuario no existe verifica tu nombre de usuario")
-				.setCancelable(false)
-				.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						dialog.cancel();
-					}
-				});*/
-				Toast.makeText(mActivity, "El usuario no existe verifica tu nombre de usuario", Toast.LENGTH_LONG).show();
-			}
-		} catch (NullPointerException e) {
-			/*builder.setMessage("No hay conexion al servidor")
-			.setCancelable(false)
-			.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int id) {
-					dialog.cancel();
-				}
-			});*/
-			Toast.makeText(mActivity, "No hay conexion al servidor", Toast.LENGTH_LONG).show();
+	/**
+	 * on getting result
+	 */
+
+	protected void onPostExecute(ResponseFromHttpPost response) {
+		try{
+		if (response.getStatusCode() == 200) {
+			this.dialog.dismiss();
+			Toast.makeText(mActivity, "Se ha enviado la oferta al servidor",
+					Toast.LENGTH_LONG).show();
+		}}
+		catch (Exception e) {
+			Toast.makeText(mActivity, "No hay conexion al servidor",
+					Toast.LENGTH_LONG).show();
 			e.printStackTrace();
+			
 		}
-    	/*AlertDialog alertDialog = builder.create();
-		alertDialog.show();
-		return;*/
-    }
+
+
+		// AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+//		try {
+//			if (response.getStatusCode() == 200) {
+				// builder.setMessage("Se ha enviado la oferta al servidor")
+				// .setCancelable(false)
+				// .setNegativeButton("Ok",
+				// new DialogInterface.OnClickListener() {
+				// public void onClick(DialogInterface dialog,
+				// int id) {
+				// dialog.cancel();
+				// }
+				// });
+				// Toast.makeText(mActivity,
+				// "Se ha enviado la oferta al servidor",
+				// Toast.LENGTH_LONG).show();
+//			}
+//			if (response.getStatusCode() == 400) {
+				/*
+				 * builder.setMessage(
+				 * "Tu oferta no ha podido ser enviada, intenta nuevamente")
+				 * .setCancelable(false) .setNegativeButton("Ok", new
+				 * DialogInterface.OnClickListener() { public void
+				 * onClick(DialogInterface dialog, int id) { dialog.cancel(); }
+				 * });
+				 */
+//				Toast.makeText(
+//						mActivity,
+//						"Tu oferta no ha podido ser enviada, intenta nuevamente",
+//						Toast.LENGTH_LONG).show();
+//			}
+//			if (response.getStatusCode() == 500) {
+				/*
+				 * builder.setMessage(
+				 * "El usuario no existe verifica tu nombre de usuario")
+				 * .setCancelable(false) .setNegativeButton("Ok", new
+				 * DialogInterface.OnClickListener() { public void
+				 * onClick(DialogInterface dialog, int id) { dialog.cancel(); }
+				 * });
+				 */
+//				Toast.makeText(mActivity,
+//						"El usuario no existe verifica tu nombre de usuario",
+//						Toast.LENGTH_LONG).show();
+//			}
+//		} catch (NullPointerException e) {
+			/*
+			 * builder.setMessage("No hay conexion al servidor")
+			 * .setCancelable(false) .setNegativeButton("Ok", new
+			 * DialogInterface.OnClickListener() { public void
+			 * onClick(DialogInterface dialog, int id) { dialog.cancel(); } });
+			 */
+//			Toast.makeText(mActivity, "No hay conexion al servidor",
+//					Toast.LENGTH_LONG).show();
+//			e.printStackTrace();
+//		}
+		/*
+		 * AlertDialog alertDialog = builder.create(); alertDialog.show();
+		 * return;
+		 */
+	}
+
+	@Override
+	protected void onProgressUpdate(String... values) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+
+		super.onProgressUpdate(values);
+	}
 }
--- src/com/androidtitlan/endeavorsubasta/util/resources.java
+++ src/com/androidtitlan/endeavorsubasta/util/resources.java
@@ -1,8 +1,8 @@
 package com.androidtitlan.endeavorsubasta.util;
 
 public class Resources {
-//	public static final String URL_SUBASTA = "http://10.0.1.112:8000/endeavor/";
+	public static final String URL_SUBASTA = "http://10.0.1.112:8000/endeavor/";
 //	public static final String URL_SUBASTA = "http://4ira.localtunnel.com/endeavor/";
-	public static final String URL_SUBASTA = "http://192.168.0.17:8000/endeavor/";
-	public static final String APP_NAME = "Subasta Gala Endeavor 2012";
+//	public static final String URL_SUBASTA = "http://192.168.1.175:8000/endeavor/";
+	public static final String APP_NAME = "Subasta Gala Endeavor 2012"; 
 }
